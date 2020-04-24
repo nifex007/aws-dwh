e789e16b1b8bb57332a32fc3cb8c1b85c378e492
Nifemi Sola-Ojo <nifemi@ashnelsonpartners.com>
2020-04-24 11:02:22 +0100

    creates tables

--- create_tables.py
+++ create_tables.py
@@ -6,12 +6,14 @@ from sql_queries import create_table_queries, drop_table_queries
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
+        print('Dropping Tables')
         conn.commit()
 
 
 def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
+        print(query, 'Created!')
         conn.commit()
 
 
